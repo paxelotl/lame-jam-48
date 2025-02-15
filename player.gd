463557cb2194dbe1cec52c9f65b55b02fabb62ec
paxelotl <141200190+paxelotl@users.noreply.github.com>
2025-02-14 23:14:24 -0800

    Initial commit

---
+++ player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+const SPEED = 180.0
+@onready var arrow_pivot = $ArrowPivot
+@onready var arrow = $ArrowPivot/Arrow
+
+var direction_to_arrow: Vector2
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		match event.button_index:
+			MOUSE_BUTTON_LEFT:
+				velocity = direction_to_arrow.normalized() * 800
+
+func _physics_process(delta: float) -> void:
+	# point arrow towards cursor
+	arrow_pivot.look_at(get_global_mouse_position())
+	
+	# Get dir to arrow
+	direction_to_arrow = arrow.global_position - global_position
+	
+	# Fall down
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Handle left right input
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, SPEED / 3)
+
+	move_and_slide()
