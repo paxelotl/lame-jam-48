19ccaf0e548047d3a94efd7f9538c8890081d550
paxelotl <141200190+paxelotl@users.noreply.github.com>
2025-02-16 20:18:09 -0800

    Hide void beneath ground

--- camera.gd
+++ camera.gd
@@ -3,4 +3,6 @@ extends Camera2D
 @onready var player = $"../Player"
 
 func _process(delta: float) -> void:
-	position = player.position
+	position.x = player.position.x
+	if player.global_position.y < 360:
+		position.y = player.position.y
