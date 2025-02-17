ef73a2f85c5a3da9f62e806d3e3a3ad40b44c59a
paxelotl <141200190+paxelotl@users.noreply.github.com>
2025-02-16 22:58:38 -0800

    Add score counter

--- camera.gd
+++ camera.gd
@@ -1,8 +1,18 @@
 extends Camera2D
 
 @onready var player = $"../Player"
+@onready var score = $"Score"
+var high_score = 0
 
 func _process(delta: float) -> void:
 	position.x = player.position.x
 	if player.global_position.y < 360:
 		position.y = player.position.y
+	
+	update_score()
+
+func update_score():
+	var current_score = (player.position.y - 540) / 360 * -1
+	if current_score > high_score:
+		high_score = current_score
+	score.text = str(floor(high_score))
